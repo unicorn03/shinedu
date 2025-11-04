b9165d3365c9c3524bdc7fc0c178e6a43e5c1a44
Ramadhan Cahaya <bimarama2014@gmail.com>
2025-11-04 15:42:01 +0700

    a

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -10,17 +10,24 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <script src="https://cdn.tailwindcss.com"></script>
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
+        <nav class="bg-white shadow-sm">
+            <div class="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
+                <a href="#" class="text-2xl font-semibold text-blue-800">Shinedu</a>
+      
+                <div class="flex space-x-3">
+                    <button class="bg-blue-700 text-white font-semibold px-4 py-2 rounded-md hover:bg-blue-800 transition">Login</button>
+                    <button class="bg-gray-100 text-blue-800 font-semibold px-4 py-2 rounded-md hover:bg-gray-200 transition">Sign Up</button>
+                </div>
+
             </div>
+        </nav>
+        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
 
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
