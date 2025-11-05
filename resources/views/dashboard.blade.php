54783a40b830491e2739c1157a257a36ca264618
dew <dewagederafel@gmail.com>
2025-11-05 09:25:59 +0700

    iya

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,8 +4,8 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                <div class="text-[45px] text-blue-800 text-center ">
+                    {{ __("AKU SHINEDU") }}
                 </div>
             </div>
         </div>
